41f5e083a0a96c5d1adccfda323dfa76996ee1a9
서동경 <ehhdrud@icloud.com>
2024-03-07 22:28:05 +0900

    feature: [feature] - 하단 네비게이션 추가

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import ShortTermForecast from '../screens/ShortTermForecast';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            {/* <Stack.Navigator>
                 <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-            </Stack.Navigator>
+            </Stack.Navigator> */}
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+                <Tab.Screen name="Short-term Forecast" component={ShortTermForecast} />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 }
---
+++ screens/ShortTermForecast.js
@@ -0,0 +1,10 @@
+// 기상청 단기예보 API
+import { Text, View } from 'react-native';
+
+export default function ShortTermForecast() {
+    return (
+        <View>
+            <Text>Short-term Weather Forecast</Text>
+        </View>
+    );
+}
