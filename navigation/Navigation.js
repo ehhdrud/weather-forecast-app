1bc34b3283f800af1c440ecea348cfb1ce40c561
서동경 <ehhdrud@icloud.com>
2024-03-08 23:40:57 +0900

    feature: [feature] - 하단 네비게이션 아이콘 변경

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -2,21 +2,40 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import HomeScreen from '../screens/HomeScreen';
 import ShortTermForecast from '../screens/ShortTermForecast';
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
     return (
         <NavigationContainer>
-            {/* <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-            </Stack.Navigator> */}
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-                <Tab.Screen name="Short-term Forecast" component={ShortTermForecast} />
+            <Tab.Navigator initialRouteName="Home">
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="earth" size={size} color={color} />
+                        ),
+                        headerShown: false,
+                    }}
+                />
+                <Tab.Screen
+                    name="Short-term Forecast"
+                    component={ShortTermForecast}
+                    options={{
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="weather-cloudy-clock"
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                        // headerShown: false,
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     );
